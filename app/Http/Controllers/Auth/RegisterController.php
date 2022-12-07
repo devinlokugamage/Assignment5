d2ad5456c4b731888aefff8367fe42f4201b51b6
Your Name <you@example.com>
2022-12-06 23:15:57 -0500

    Intial Files Commit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::patient_home;
 
     /**
      * Create a new controller instance.
